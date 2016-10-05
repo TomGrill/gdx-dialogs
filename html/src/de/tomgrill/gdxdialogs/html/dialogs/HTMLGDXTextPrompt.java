d008738dad4ed08c01cc73a2f82c219073bdc450
fxzjshm <fxzjshm@163.com>
2016-10-05 15:59:54 +0800

    Fixed a small mistake.

--- html/src/de/tomgrill/gdxdialogs/html/dialogs/HTMLGDXTextPrompt.java
+++ html/src/de/tomgrill/gdxdialogs/html/dialogs/HTMLGDXTextPrompt.java
@@ -1,12 +1,12 @@
 /*******************************************************************************
  * Copyright 2015 See AUTHORS file.
- * <p>
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * <p>
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * <p>
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -181,6 +181,7 @@ public class HTMLGDXTextPrompt implements GDXTextPrompt {
 
         var confirmNode = $doc.createElement('input');
         confirmNode.id = id + "-confirm";
+        confirmNode.type = "button"
         confirmNode.style = "font-size: 1em; margin-top: 10px;";
         confirmNode.value = confirm;
         var confirmAction = function() {
